17a4a48732c4d9d05904e85b92961e54fbdee67b
Yang Long <longyang_123@yeah.net>
2018-02-26 16:27:07 +0800

    add mem op code

--- CheatEngine/CheatEngine/MemoryManager.cs
+++ CheatEngine/CheatEngine/MemoryManager.cs
@@ -15,6 +15,85 @@ namespace CheatEngine
             Process[] ps = Process.GetProcesses();
             return ps;
         }
-        
+
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int MEM_COMMIT = 0x00001000;
+        const int PAGE_READWRITE = 0x04;
+        const int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int VirtualQueryEx
+            (
+                IntPtr hProcess,
+                IntPtr lpAddress, 
+                out MEMORY_BASIC_INFORMATION lpBuffer, 
+                uint dwLength
+            );
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImportAttribute("kernel32.dll", EntryPoint = "ReadProcessMemory")]
+        public static extern bool ReadProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                IntPtr lpBuffer,
+                int nSize,
+                IntPtr lpNumberOfBytesRead
+            );
+
+        [DllImportAttribute("kernel32.dll", EntryPoint = "OpenProcess")]
+        public static extern IntPtr OpenProcess
+            (
+                int dwDesiredAccess,
+                bool bInheritHandle,
+                int dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        private static extern void CloseHandle
+            (
+                IntPtr hObject
+            );
+
+        [DllImportAttribute("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+        public static extern bool WriteProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                int[] lpBuffer,
+                int nSize,
+                IntPtr lpNumberOfBytesWritten
+            );
+
+        [DllImport("kernel32")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public struct MEMORY_BASIC_INFORMATION
+        {
+            public int BaseAddress;
+            public int AllocationBase;
+            public int AllocationProtect;
+            public int RegionSize;
+            public int State;
+            public int Protect;
+            public int lType;
+        }
+
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public IntPtr minimumApplicationAddress;
+            public IntPtr maximumApplicationAddress;
+            public IntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
     }
 }
